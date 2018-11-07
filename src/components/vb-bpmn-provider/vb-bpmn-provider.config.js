b5a840dade4ab4bcd70a3a01c3a47407f8e907c1
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2018-11-07 17:05:39 +0300

    Create vb-bpmn-provider.config.js

---
+++ src/components/vb-bpmn-provider/vb-bpmn-provider.config.js
@@ -0,0 +1,13 @@
+export const urlBase =
+    '/api/vb-bpmn/';
+
+// Шаблон bpmn - отправная точка для создания новой схемы
+export const urlDefaultBpmn =
+    urlBase + 'default.bpmn';
+
+// Путь к "каталогу файлов россыпью", входная точка для сервиса
+export const urlBpmnList = 
+    urlBase + 'list/';
+
+export const defaultBranch =
+    'master';
