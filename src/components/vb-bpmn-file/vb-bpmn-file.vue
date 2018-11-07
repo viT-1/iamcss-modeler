fd79581eb34dc6cc481e5464b177693cadf2d0ea
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2018-11-07 16:50:01 +0300

    Create vb-bpmn-file.vue

---
+++ src/components/vb-bpmn-file/vb-bpmn-file.vue
@@ -0,0 +1,40 @@
+<style src="./vb-bpmn-file.css"></style>
+
+<template>
+    <span class="vb-bpmn-file" @click="nameClick()">{{ metaData.name }}</span>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    import appStore from '@/components/vb-bpmn-modeler-app-store/';
+
+    export default {
+        name: 'vbBpmnFile',
+        props: {
+            metaData: {
+                type: Object, // { name: val }
+                required: true,
+                validator: function( data ){
+                    return data.hasOwnProperty( 'name' );
+                }
+            }
+        },
+        methods: Object.assign( {},
+            mapActions({
+                _nameClick: 'bpmnFile/nameClick'
+                // _requestBpmn: 'provider/requestXml'
+            }),
+            {
+                // nameClick: function(){
+                //     this._requestBpmn( this.metaData )
+                //         .then( function( resp ){
+                //             appStore.commit( 'modeler/setActiveDiagram', resp.data );
+                //         });
+                // }
+                nameClick: function(){
+                    this._nameClick( this.metaData );
+                }
+            }
+        )
+    }
+</script>
