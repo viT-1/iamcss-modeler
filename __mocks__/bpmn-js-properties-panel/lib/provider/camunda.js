a23edddcdd06b416aff477c2877cfda8e4a76b74
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2018-11-07 16:37:06 +0300

    Create camunda.js

---
+++ __mocks__/bpmn-js-properties-panel/lib/provider/camunda.js
@@ -0,0 +1,7 @@
+// API выдрано из bpmn-js-properties-panel/lib/provider/camunda/CamundaPropertiesProvider
+function CamundaPropertiesProvider( eventBus, bpmnFactory, elementRegistry, elementTemplates, translate ){};
+
+console.log( '__mocks__/bpmn-js-properties-panel/lib/provider/camunda' );
+
+module.exports = CamundaPropertiesProvider;
+// Экономия 8 секунд!
