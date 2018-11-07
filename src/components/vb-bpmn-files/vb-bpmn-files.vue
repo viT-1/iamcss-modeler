79faba693aead3e3e07610fe8e37fa3b8b047055
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2018-11-07 16:52:58 +0300

    Create vb-bpmn-files.vue

---
+++ src/components/vb-bpmn-files/vb-bpmn-files.vue
@@ -0,0 +1,42 @@
+<style src="./vb-bpmn-files.css"></style>
+
+<template>
+    <div>
+        <ul v-iam-element = "'list'"
+            v-if = "items && items.length"
+
+            ><li v-iam-element = "'item'"
+                v-for = "item in items"
+
+                ><vb-bpmn-file :meta-data = "item" />
+            </li>
+        </ul>
+        <vb-message
+            v-else
+            text = "Bpmn-файлы отсутствуют"
+            iam-context = "vb-bpmn-files"/>
+    </div>
+</template>
+
+<script>
+    import vbMessage from '@/components/vb-message/vb-message.vue';
+    import vbBpmnFile from '@/components/vb-bpmn-file/vb-bpmn-file.vue';
+
+    import iAMcssMix from '@/components/iAMcss/iAMcss.mixin.js';
+    import { iamElement } from '@/components/iAMcss/iAMcss.directives.js';
+
+    export default {
+        name: 'vb-bpmn-files',
+        mixins: [ iAMcssMix ],
+        props: {
+            items: {
+                type: Array,
+                default: () => []
+            }
+        },
+        components: {
+            'vb-bpmn-file': vbBpmnFile,
+            'vb-message': vbMessage
+        }
+    }
+</script>
